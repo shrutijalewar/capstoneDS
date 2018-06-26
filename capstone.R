1605d14320e6730a6ca86405f0dae736b0c6961d
Shruti Sharma <shrutijalewar@gmail.com>
2018-06-26 18:48:40 -0500

    adding code for cleaning and initial sentiment analysis

---
+++ capstone.R
@@ -0,0 +1,15 @@
+setwd('/Users/ssharma/code/nss-ds/capstone/')
+library("jsonlite")
+library("tidyverse")
+library("dplyr")
+library("magrittr")
+library("ggplot2")
+library("readxl")
+
+###### Reading Data######
+news <-  read_csv('all_news.csv')
+
+##### what the hell ####
+
+
+
